a1c9cb64ec8e7826c72dafb2c19f463b683fa3f2
Yuda <akuntestinguntukseto@gmail.com>
2019-02-02 15:18:20 +0700

    init

---
+++ test/flutter_collapsable_toolbar_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_collapsable_toolbar/flutter_collapsable_toolbar.dart';
+
+void main() {
+  test('adds one to input values', () {
+    //
+  });
+}
